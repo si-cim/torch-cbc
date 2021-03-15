9ba3da5e49177ba647dd816c7cdd5ced8a3008f4
Jensun Ravichandran <jjensun@gmail.com>
2021-03-15 13:55:47 +0100

    Add fashion_mnist script

--- torch_cbc/cbc_model.py
+++ torch_cbc/cbc_model.py
@@ -1,41 +1,37 @@
+import copy
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import copy
 
-from .layers import ReasoningLayer
 from .cosine2d import CosineSimilarity2D
+from .layers import ReasoningLayer
 
 
 class CBCModel(nn.Module):
-    def __init__(self, backbone, n_classes, n_components, component_shape, reasoning_init='random'):
+    def __init__(self,
+                 backbone,
+                 n_classes,
+                 n_components,
+                 component_shape,
+                 reasoning_init='random'):
         super(CBCModel, self).__init__()
-
         self.backbone = backbone
-
         self.components = nn.Parameter(  # (n_components, H, W)
-            torch.rand((n_components,) + component_shape).uniform_(0.45, 0.55)
-        )
-
+            torch.rand((n_components, ) + component_shape).uniform_(
+                0.45, 0.55))
         # get size of feature map that will be input for the Cosine2D layer
         featuremap_shape = self.backbone(
-            torch.zeros((1,) + component_shape)).shape[1:]
-
-        self.similarity = CosineSimilarity2D(filter_size=(n_components,) + featuremap_shape)  # noqa
-
+            torch.zeros((1, ) + component_shape)).shape[1:]
+        self.similarity = CosineSimilarity2D(filter_size=(n_components, ) +
+                                             featuremap_shape)  # noqa
         self.reasoning_layer = ReasoningLayer(n_components=n_components,
                                               n_classes=n_classes,
                                               initialization=reasoning_init)
 
     def forward(self, x):
-
         x = self.backbone(x)
         y = copy.deepcopy(self.backbone)(self.components)
-
-        detection = self.similarity(x, y)
-        # detection: (batch, n_components, 1, 1)
-
-        detection = detection.squeeze(-1).squeeze(-1)
-        # detection: (batch size, n_components)
-
+        detection = self.similarity(x, y) # detection: (batch, n_components, 1, 1)
+        detection = detection.squeeze(-1).squeeze(-1) # detection: (batch size, n_components)
         return self.reasoning_layer(detection)
--- utils.py
+++ utils.py
@@ -1,29 +1,28 @@
 import os
-from torchvision.transforms import ToPILImage
+
 from PIL import Image, ImageOps
+from torchvision.transforms import ToPILImage
 
 
 def get_concat_h(images):
-    dst = Image.new('RGB', (len(images)*(64+8), 64+8))
+    dst = Image.new("RGB", (len(images) * (64 + 8), 64 + 8))
     for i, im in enumerate(images):
-        dst.paste(im, (i*(64+8), 0))
+        dst.paste(im, (i * (64 + 8), 0))
     return dst
 
 
-def visualize_components(epoch, model, save_path):
+def visualize_components(epoch, model, save_path, verbose=True):
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-
     images = []
     for idx, component in enumerate(model.components):
-
         image = ToPILImage()(component.cpu()).convert("RGB")
-
         image = image.resize((64, 64))
         # image.save(f"{save_path}/{epoch}_{idx}.png")
-
-        image = ImageOps.expand(image, border=4, fill='black')
+        image = ImageOps.expand(image, border=4, fill="black")
         images.append(image)
-
     result = get_concat_h(images)
-    result.save(f"{save_path}/{epoch}.png")
+    filename = f"{save_path}/{epoch}.png"
+    result.save(filename)
+    if verbose:
+        print(f"Saved components image from epoch {epoch} under {filename}.")
